2fb41fcbadb556d3ce065a480eae2a74c4a337f8
savithamk <savitha.maiya1@gmail.com>
2022-08-14 21:35:26 -0400

    Created Radio Buttons to select multiple die types
    Added two buttons to display die results and added functionality
    Once "Display one result" clicked, internally calls Roll method and display result in  using first label and hides next label and visa versa.

---
+++ DiceRoller-UI-Tests/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace DiceRollerUITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+                return ConfigureApp
+                .Android
+                .InstalledApp("com.savitha.diceroller")
+                .StartApp();  
+        }
+    }
+}
--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -5,25 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using DiceRoller.Models;
 
 namespace DiceRoller
 {
     public partial class MainPage : ContentPage
     {
+        private int numOfSides = 0;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-        public void DisplayOne()
+        
+        void Display_One_Handler(System.Object sender, System.EventArgs e)
         {
-          //  Display 1 result
-                //hide label 2
+
+            if (numOfSides > 0) {
+                Die die = new Die(numOfSides);
+                Result2.IsVisible = false;
+                Result1.IsVisible = true;
+                Result1.Text = die.GetCurrentSide().ToString();
+            } 
+            
         }
-        public void DisplayTwo()
+
+        void Display_Two_Handler(System.Object sender, System.EventArgs e)
         {
-            //  Display 2 results
-            //hide label 1
+            if (numOfSides > 0) {
+                Die die1 = new Die(numOfSides);
+                Die die2 = new Die(numOfSides);
+                Result1.IsVisible = false;
+                Result2.IsVisible = true;
+                Result2.Text = die1.GetCurrentSide().ToString() + "," + die2.GetCurrentSide().ToString();
+            }
+            
+        }
+
+        void Die_Selected_Handler(System.Object sender, Xamarin.Forms.CheckedChangedEventArgs e)
+        {
+            RadioButton changed = (RadioButton)sender;
+            if (changed.IsChecked) {
+                numOfSides = Int32.Parse(changed.Value.ToString());
+            }
+            
+
         }
     }
 }
